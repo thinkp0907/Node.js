51b89b6af8842b09cb141dddd60f59eff0a28a0d
DongGyun.Cho <thinkp0907@gmail.com>
2021-01-11 18:09:02 +0900

    javascirpt_basic_syntax

---
+++ syntax/Variable2.js
@@ -0,0 +1,5 @@
+var name = 'k8805'
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '+name+' quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum  egoing dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non '+name+' proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+// data에 이름 설정!
+console.log(letter);
